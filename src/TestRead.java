ac4828334b8b47d75e791ecfb1c2d4805f2ed48c
evandrolopes85 <52211865+evandrolopes85@users.noreply.github.com>
2019-11-06 08:17:04 -0300

    Update TestRead.java

--- src/TestRead.java
+++ src/TestRead.java
@@ -3,16 +3,16 @@ public class TestRead{
 		UsuarioDao dao = new UsuarioDao();
 
 		Usuario u = new Usuario();
-		u.setNome("Nete");
-		u.setEmail("pequenaluzi@hotmail.com");
+		u.setNome("Usuario");
+		u.setEmail("usuario@hotmail.com");
 		u.setEndereco("R. Leandro Campanario, 3A");
 		u.setDataNascimento(java.time.LocalDate.of(2019,01,22));
 		dao.create(u);
 		
 		Usuario incompleto = new Usuario();
-		incompleto.setNome("Nete");
+		incompleto.setNome("Usuario");
 
-		//incompleto.setEmail("evandrocwd@gmail.com");
+		//incompleto.setEmail("email");
 
 		//dao.delete(incompleto);
 
