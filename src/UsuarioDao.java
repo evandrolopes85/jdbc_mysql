305971e48e8785c1d04039016e1f46cb0cc457e1
evandrolopes85 <evandrocwd@gmail.com>
2019-11-12 07:23:15 -0300

    Correcao de Erros

--- src/UsuarioDao.java
+++ src/UsuarioDao.java
@@ -13,46 +13,62 @@ public class UsuarioDao implements GenericDao{
 	}
 	
 	public void create(Object o){
-		Usuario usuario = (Usuario)o;
 
-		String sql = "insert into contatos (nome, email, endereco, dataNascimento) values (?,?,?,?)";
-		try{
-			PreparedStatement stmt = connection.prepareStatement(sql);
-			stmt.setString(1, usuario.getNome());
-			stmt.setString(2, usuario.getEmail());
-			stmt.setString(3, usuario.getEndereco());
-			stmt.setDate(4, java.sql.Date.valueOf(usuario.getDataNascimento()));
-			stmt.execute();
+		if(o instanceof Usuario){
+			Usuario usuario = (Usuario)o;
 
-			System.out.println("Usuario cadastrado!");
-			stmt.close();
-		}catch(SQLException e){
-			System.err.println("Erro ao cadastrar!" + e.getMessage());
+			String sql = "insert into contatos (nome, email, endereco, dataNascimento) values (?,?,?,?)";
+			try{
+				PreparedStatement stmt = connection.prepareStatement(sql);
+				stmt.setString(1, usuario.getNome());
+				stmt.setString(2, usuario.getEmail());
+				stmt.setString(3, usuario.getEndereco());
+				stmt.setDate(4, java.sql.Date.valueOf(usuario.getDataNascimento()));
+				stmt.execute();
+
+				System.out.println("Usuario cadastrado!");
+				stmt.close();
+			}catch(SQLException e){
+				System.err.println("Erro ao cadastrar!" + e.getMessage());
+			}
+		}else{
+			throw new RuntimeException("Invalid Object");
 		}
+		
 	}
 
 	public Object read(Object o){
-		Usuario incompleto = (Usuario) o;
-		try{
-			String sql = "select * from contatos where nome=? or email=?";
-			PreparedStatement stmt = connection.prepareStatement(sql);
-			stmt.setString(1, incompleto.getNome());
-			stmt.setString(2, incompleto.getEmail());
-			ResultSet rs = stmt.executeQuery();
-			while(rs.next()){
-				incompleto.setId(rs.getInt("id"));
-				incompleto.setNome(rs.getString("nome"));
-				incompleto.setEmail(rs.getString("email"));
-				incompleto.setEndereco(rs.getString("endereco"));
-				incompleto.setDataNascimento(rs.getDate("dataNascimento").toLocalDate());
+		if(o instanceof Usuario){
+			Usuario incompleto = (Usuario) o;
+			try{
+				String sql = "select * from contatos where nome=? or email=?";
+				PreparedStatement stmt = connection.prepareStatement(sql);
+				stmt.setString(1, incompleto.getNome());
+				stmt.setString(2, incompleto.getEmail());
+				ResultSet rs = stmt.executeQuery();
+			
+				Usuario usuario = new Usuario();
+
+				while(rs.next()){
+					usuario.setId(rs.getInt("id"));
+					usuario.setNome(rs.getString("nome"));
+					usuario.setEmail(rs.getString("email"));
+					usuario.setEndereco(rs.getString("endereco"));
+					usuario.setDataNascimento(rs.getDate("dataNascimento").toLocalDate());
+				}
+
+				stmt.close();
+				rs.close();
+				return usuario; 
+			}catch(SQLException e){
+				System.err.println("Erro ao recuperar dados " + e.getMessage());
 			}
 
-			stmt.close();
-			rs.close();
-		}catch(SQLException e){
-			System.err.println("Erro ao recuperar dados " + e.getMessage());
+		}else{
+			throw new RuntimeException("Invalid Object");
 		}
-		return incompleto;
+
+		return null;
 	}
 
 	public void update(Object o){
